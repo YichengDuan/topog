d32a2c46a0fe54545c58b6646d76d02174276af9
Yicheng D <yxd245@case.edu>
2025-04-17 23:20:13 -0400

    update vision_gd

--- vision_gd.py
+++ vision_gd.py
@@ -1,34 +1,26 @@
-import requests
-
+from transformers import DetrImageProcessor, DetrForObjectDetection
 import torch
 from PIL import Image
-from transformers import AutoProcessor, AutoModelForZeroShotObjectDetection
+import requests
 
-device = "mps" if torch.backends.mps.is_available() else "cuda" if torch.cuda.is_available() else "cpu"
+url = "./data/graph/17DRP5sb8fy/17DRP5sb8fy_level0/img/17DRP5sb8fy_level0_1_2.jpg"
+image = Image.open(url)
 
-model_id = "./model/grounding-dino-tiny"
+# you can specify the revision tag if you don't want the timm dependency
+processor = DetrImageProcessor.from_pretrained("facebook/detr-resnet-50", revision="no_timm")
+model = DetrForObjectDetection.from_pretrained("facebook/detr-resnet-50", revision="no_timm")
 
-processor = AutoProcessor.from_pretrained(model_id)
-model = AutoModelForZeroShotObjectDetection.from_pretrained(model_id).to(device)
+inputs = processor(images=image, return_tensors="pt")
+outputs = model(**inputs)
 
-image_url = "http://images.cocodataset.org/val2017/000000039769.jpg"
-image = Image.open(requests.get(image_url, stream=True).raw)
-# Check for cats and remote controls
-text_labels = [["a cat", "a remote control"]]
+# convert outputs (bounding boxes and class logits) to COCO API
+# let's only keep detections with score > 0.9
+target_sizes = torch.tensor([image.size[::-1]])
+results = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0.9)[0]
 
-inputs = processor(images=image, text=text_labels, return_tensors="pt").to(device)
-with torch.no_grad():
-    outputs = model(**inputs)
-
-results = processor.post_process_grounded_object_detection(
-    outputs,
-    inputs.input_ids,
-    box_threshold=0.4,
-    text_threshold=0.3,
-    target_sizes=[image.size[::-1]]
-)
-
-result = results[0]
-for box, score, labels in zip(result["boxes"], result["scores"], result["labels"]):
-    box = [round(x, 2) for x in box.tolist()]
-    print(f"Detected {labels} with confidence {round(score.item(), 3)} at location {box}")
+for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
+    box = [round(i, 2) for i in box.tolist()]
+    print(
+            f"Detected {model.config.id2label[label.item()]} with confidence "
+            f"{round(score.item(), 3)} at location {box}"
+    )
